bd1dc0aa70743f40b32ed74521de41dcef3a7d78
Zac Seales <seaza886@student.otago.ac.nz>
2022-09-29 18:22:30 +1300

    Create account design complete.
    References #8.

--- static/js/navigation-menu.js
+++ static/js/navigation-menu.js
@@ -15,14 +15,14 @@ export const navigationMenu = {
     template:
     `
     <nav>
-        <div v-if="signedIn">Welcome {{customer.firstName}}</div>
+        <div class="font6" v-if="signedIn">--- Welcome {{customer.firstName}} ---</div>
         <p></p>
-        <a href="index.html">Home</a>
-        <a href="browse.html" v-if="signedIn">Browse Products</a>
-        <a href="view-cart.html" v-if="signedIn">View Cart</a>
-        <a href="#" v-if="signedIn" @click="signOut()">Sign Out</a>
-        <a href="create-account.html" v-if="!signedIn">Create Account</a>
-        <a href="sign-in.html" v-if="!signedIn">Sign In</a>
+        <a class="font2" href="index.html">Home</a>
+        <a class="font2" href="browse.html" v-if="signedIn">Browse Products</a>
+        <a class="font2" href="view-cart.html" v-if="signedIn">View Cart</a>
+        <a class="font2" href="#" v-if="signedIn" @click="signOut()">Sign Out</a>
+        <a class="font2" href="create-account.html" v-if="!signedIn">Create Account</a>
+        <a class="font2" href="sign-in.html" v-if="!signedIn">Sign In</a>
         <p></p>
     </nav>
     `,
